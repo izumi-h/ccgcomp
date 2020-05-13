c012f3fc63fab29f3dcb85da442583162d767f19
Izumi Haruta <haruta.izumi@is.ocha.ac.jp>
2020-05-13 22:32:15 +0900

    modified

--- tools/install_parsers.sh
+++ tools/install_parsers.sh
@@ -4,7 +4,6 @@
 #
 candc_url_linux="https://drive.google.com/uc?id=1MAqE0RmAC1sOW6A9ErpQcFmFbzD66i7x"
 candc_url_mac="https://drive.google.com/uc?id=1vl9rwQDqhy5dmt8D8EnEPTGw9zqK0KSz"
-# models_url="https://drive.google.com/uc?id=1LR6h3rX7a4Dq7fV_bc2mEmeYxteSyenH"
 
 if [ "$1" = "mac" ]; then
   candc_url=$candc_url_mac
@@ -15,18 +14,24 @@ else
   exit
 fi
 
-models_url="https://github.com/chrzyki/candc/raw/master/models/models-1.02.tgz"
+# models_url="https://drive.google.com/uc?id=1LR6h3rX7a4Dq7fV_bc2mEmeYxteSyenH"
+models_id=1LR6h3rX7a4Dq7fV_bc2mEmeYxteSyenH
 parser_basename=`basename $candc_url`
-models_basename=`basename $models_url`
+# models_basename=`basename $models_url`
+models_basename=models-1.02.tgz
 
 test -d candc-1.00 || {
   test -f $parser_basename || {
     wget $candc_url
   }
 
-  test -f $models_url || {
-    wget $models_url
-  }
+  # test -f $models_url || {
+  #     wget $models_url
+  # }
+
+  curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${models_id}" > /dev/null
+  CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
+  curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${models_id}" -o $models_basename
 
   tar xvzf $parser_basename
   tar xvzf $models_basename --directory candc-1.00
